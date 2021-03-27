6660da68b89b2c5fed2b0a68bc05366a33028ea0
SmariF89 <smarifreyr30@gmail.com>
2021-03-27 10:48:41 +0000

    Refactored QC folder and added an option to train_accustic_model to input a list of ids to use for training.

--- QualityCheck/train_accustic_model_slurm.sh
+++ QualityCheck/train_accustic_model_slurm.sh
@@ -5,4 +5,8 @@
 #SBATCH --mem=12G
 #SBATCH --output=training.log
 
+#SBATCH --nodelist=terra
+
 python3 train_accustic_model.py
+
+# 230321 #1: sbatch -o tr_slurm_logs/230321_training_01.log train_accustic_model_slurm.sh
